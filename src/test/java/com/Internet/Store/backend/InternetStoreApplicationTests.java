e11df608c7a056d751bb607a3b7281e0245d2bca
YuriyPetsa <81989046+YuriyPetsa@users.noreply.github.com>
2023-09-26 08:39:59 +0300

    init project

---
+++ src/test/java/com/Internet/Store/backend/InternetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Internet.Store.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
